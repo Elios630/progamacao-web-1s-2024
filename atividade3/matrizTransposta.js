b273328dd1298cb8d134b54a6bff5545b7fdd5e3
Elios630 <78980830+Elios630@users.noreply.github.com>
2024-03-27 11:40:42 -0300

    Update matrizTransposta.js

--- atividade3/matrizTransposta.js
+++ atividade3/matrizTransposta.js
@@ -1,16 +1,22 @@
+function transporMatriz(A) {
+
+    const transposta = [];
+    for (let i = 0; i < A[0].length; i++) {
+        transposta[i] = [];
+        for (let j = 0; j < A.length; j++) {
+            transposta[i][j] = A[j][i];
+        }
+    }
+    return transposta;
+}
+
 const matrizOriginal = [
     [1, 2],
     [3, 4],
     [5, 6]
 ];
 
-const transposta = [];
-for (let i = 0; i < matrizOriginal[0].length; i++) {
-    transposta[i] = [];
-    for (let j = 0; j < matrizOriginal.length; j++) {
-        transposta[i][j] = matrizOriginal[j][i];
-    }
-}
+transposta = transporMatriz(matrizOriginal);
 
 console.log("Matriz original:");
 for (let linha of matrizOriginal) {
