e2aae842b32b5a627b272fb7189ca4f6cf6ecd81
Elios630 <78980830+Elios630@users.noreply.github.com>
2024-04-11 10:25:08 -0300

    Update index.js

--- atividade4/index.js
+++ atividade4/index.js
@@ -10,32 +10,31 @@ app.get('/somar/:a/:b', function (req, res) {
     let a = req.params.a
     let b = req.params.b
     let c = calc.somar(a, b);
-    res.send(""+c);
+    res.send(`${c}`);
 });
 
 app.get('/subtrair/:a/:b', function (req, res) {
     let a = req.params.a
     let b = req.params.b
     let c = calc.subtrair(a, b);
-    res.send(""+c);
+    res.send(`${c}`);
 });
 
 app.get('/multiplicar/:a/:b', function (req, res) {
     let a = req.params.a
     let b = req.params.b
     let c = calc.multiplicar(a, b);
-    res.send(""+c);
+    res.send(`${c}`);
 });
 
 app.get('/dividir/:a/:b', function (req, res) {
     let a = req.params.a
     let b = req.params.b
     let c = calc.dividir(a, b);
-    res.send(""+c);
+    res.send(`${c}`);
 });
 
-const PORT = 8080;
+const PORT = 8000;
 app.listen(PORT, function () {
     console.log('localhost:' + PORT);
 });
-
